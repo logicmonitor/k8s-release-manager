6c688f9e0426073e9459dba61c25c005072aa34d
Jeff Wozniak <jeff.wozniak@logicmonitor.com>
2018-08-23 16:28:48 -0700

    lint fix

--- pkg/lmhelm/errors.go
+++ pkg/lmhelm/errors.go
@@ -6,6 +6,7 @@ const (
 	releaseExistsError string = "cannot re-use a name that is still in use"
 )
 
+// ErrorReleaseExists returns true if the helm error is the result of trying to reinstall and existing release
 func ErrorReleaseExists(err error) bool {
 	return strings.Contains(err.Error(), releaseExistsError)
 }
